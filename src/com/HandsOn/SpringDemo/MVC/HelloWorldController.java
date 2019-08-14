a148d845c98132c9f6f91bbe195436abef60e223
Rutva_Rajdev <rutva.rajdev@gmail.com>
2019-08-14 09:45:48 -0400

    Added contoller class and form view jsp page

---
+++ src/com/HandsOn/SpringDemo/MVC/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.HandsOn.SpringDemo.MVC;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    // Need a controller method to show initial form
+    @RequestMapping("/showForm")
+    public String showForm() {
+        return "helloworld-form";
+    }
+
+    // Need a controller method to process HTML form
+}
