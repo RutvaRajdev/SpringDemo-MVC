811125f691cf1f2eda7d84b5e218ae2a9c222a37
Rutva_Rajdev <rutva.rajdev@gmail.com>
2019-08-14 12:49:06 -0400

    Used Request param binding

--- src/com/HandsOn/SpringDemo/MVC/HelloWorldController.java
+++ src/com/HandsOn/SpringDemo/MVC/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.HandsOn.SpringDemo.MVC;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -29,6 +30,17 @@ public class HelloWorldController {
         theName = theName.toUpperCase();
 
         String result = "Hey! " + theName;
+
+        model.addAttribute("message", result);
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model){
+
+        theName = theName.toUpperCase();
+
+        String result = "Hey there! " + theName;
 
         model.addAttribute("message", result);
         return "helloworld";
--- src/main/webapp/WEB-INF/view/helloworld-form.jsp
+++ src/main/webapp/WEB-INF/view/helloworld-form.jsp
@@ -7,7 +7,7 @@
 </head>
 
 <body>
-    <form action="processFormVersionTwo" method="get">
+    <form action="processFormVersionThree" method="get">
         <label>
             <input type="text" name="studentName"
                    placeholder="What's your name?"/>
