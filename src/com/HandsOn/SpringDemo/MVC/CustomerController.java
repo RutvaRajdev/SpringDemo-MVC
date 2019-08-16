10a3eb7a980a466a391c638189dcc592aace31ef
Rutva_Rajdev <rutva.rajdev@gmail.com>
2019-08-16 14:15:37 -0400

    Added validation result check logic in controller class

--- src/com/HandsOn/SpringDemo/MVC/CustomerController.java
+++ src/com/HandsOn/SpringDemo/MVC/CustomerController.java
@@ -2,7 +2,11 @@ package com.HandsOn.SpringDemo.MVC;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/customer")
@@ -13,5 +17,19 @@ public class CustomerController {
         theModel.addAttribute("customer", new Customer());
 
         return "customer-form";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm(
+            @Valid @ModelAttribute("customer") Customer theCustomer,
+            BindingResult theBindingResult
+    ) {
+
+        if(theBindingResult.hasErrors()) {
+            return "customer-form";
+        } else {
+
+            return "customer-confirmation";
+        }
     }
 }
